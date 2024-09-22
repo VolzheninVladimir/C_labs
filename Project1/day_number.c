a5c7fd0e7f2ff1d80c65eb3f02818c7188b0c149
VolzheninVladimir <104069370+VolzheninVladimir@users.noreply.github.com>
2024-09-22 22:07:49 +0700

    day_number2

--- Project1/day_number.c
+++ Project1/day_number.c
@@ -5,10 +5,7 @@ int day_number(int day, int month, int year)
 	int list_size_months[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }, number_day = 0;
 	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) list_size_months[1] = 29;
 	if (day > list_size_months[month - 1] || day < 1 || month < 1 || month > 12) return -1;
-	for (int i = 0; i < month - 1; i++)
-	{
-		number_day += list_size_months[i];
-	}
+	for (int i = 0; i < month - 1; i++) number_day += list_size_months[i];
 	number_day += day;
 	return number_day;
 }
