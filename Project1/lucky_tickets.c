2dab5fa120d6b83d6ad6f830f73376c1d62e9b84
VolzheninVladimir <104069370+VolzheninVladimir@users.noreply.github.com>
2024-10-10 14:19:03 +0700

    Update lucky_tickets.c

--- Project1/lucky_tickets.c
+++ Project1/lucky_tickets.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
-#include <string.h>
-#include <time.h>
-
+#define ALG_SUM_9 55
 
 int main()
 {
 	float number_of_permutations = 0;
 	float total_number = 0;
-	for (int i = 0; i <= 9; i++)
-	{
-		number_of_permutations += ((i + 1.0) * (i + 2.0) / 2.0);
-		total_number += 2 * number_of_permutations * number_of_permutations;
-		number_of_permutations = 0;
-	}
+
 	for (int i = 0; i <= 3; i++)
 	{
-		number_of_permutations += 55;
-		number_of_permutations += ((18 - i) * (i + 1.0) / 2.0) - (i + 1.0) * (i+2.0) / 2.0;
+		number_of_permutations = ((18 - i) * (i + 1.0) / 2.0) - (i + 1.0) * (i + 2.0) / 2.0 + ALG_SUM_9;
 		total_number += 2 * number_of_permutations * number_of_permutations;
-		number_of_permutations = 0;	
+		number_of_permutations = ((i + 1.0) * (i + 2.0) / 2.0);
+		total_number += 2 * number_of_permutations * number_of_permutations;
 	}
+
+	for (int i = 4; i <= 9; i++)
+	{
+		number_of_permutations = ((i + 1.0) * (i + 2.0) / 2.0);
+		total_number += 2 * number_of_permutations * number_of_permutations;
+	}
+
 	printf("%f\n", total_number);
 	return 0;
 }
